f578685088d826c464a70ba8e138d5cfe7a25ce2
Douglas Carvalho <douglas.carvalho00@live.com>
2020-04-17 16:40:47 -0300

    Criação do GitHub

---
+++ src/Aluno.java
@@ -0,0 +1,14 @@
+public class Aluno{
+   private String nome;
+   private int id;
+   
+   
+public void setNome(String nome) {
+	this.nome = nome;
+}
+public void setId(int id) {
+	this.id = id;
+}
+   
+}
+   
